9baee8c35557b719e42d5eb9f6066a515a8e29b4
ssdorwat <ssdorwat@ncsu.edu>
2018-07-04 11:55:51 -0400

    added default option for select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f')  form: NgForm;
+  defaultQuestion = 'teacher';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
